4ecc239cb5deb8c7044c632855a33187eaeee56e
Abu Saeed <theyumstar@gmail.com>
2021-10-18 01:14:50 -0400

    Add base code for different line formatting

--- srt2text.py
+++ srt2text.py
@@ -1,18 +1,34 @@
 import re
 
-justreadnum = False
-justreadinterval = False
+just_read_num = False
+just_read_interval = False
+one_per_interval = True
+one_line = True
+output_line = ''
 filename = 'example.srt'
 with open(filename, 'r') as srtFile,  open('text.txt', 'w') as textFile:
-    textFile.write('\n')
     for line in srtFile:
         if re.match(r'^\d+\n$', line):
-            justreadnum = True
-            print(line.replace('\n', '', 1))
-        elif justreadnum and re.match(r'^\d{2,}:[0-5][0-9]:[0-5][0-9],\d{3} --> \d{2,}:[0-5][0-9]:[0-5][0-9],\d{3}\n$', line):
-            print(line)
-            justreadnum = False
-            justreadinterval = True
-        elif justreadinterval:
-            textFile.write(line)
+            just_read_num = True
+            just_read_interval = False
+            output_line = line.replace('\n', '', 1)
+            print(output_line)
+        elif just_read_num and re.match(r'^\d{2,}:[0-5][0-9]:[0-5][0-9],\d{3} --> \d{2,}:[0-5][0-9]:[0-5][0-9],\d{3}\n$', line):
+            output_line = line
+            print(output_line)
+            just_read_num = False
+            just_read_interval = True
+        elif just_read_interval:
+            if one_line or one_per_interval and not line in '\n':
+                output_line = line.replace('\n', '', 1) 
+                if one_line:
+                     output_line = (" %s" % output_line)        
+            else:
+                output_line = line
+            textFile.write(output_line)
+               
+        # elif justreadinterval and not line in '\n':
+        #     textFile.write(line.replace('\n', '', 1))
+        # elif oneline and justreadinterval and line in '\n':
+        #     textFile.write(line)
     srtFile.close()
