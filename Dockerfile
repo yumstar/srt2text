4f6a44152e4e6e4d0b6c562d431ff8022a66100b
Abu Saeed <theyumstar@gmail.com>
2023-01-17 03:31:32 -0500

    Set up container with mounted in/output folders

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ ENV PYTHONDONTWRITEBYTECODE=1
 # Turns off buffering for easier container logging
 ENV PYTHONUNBUFFERED=1
 
+
 # Install pip requirements
 COPY requirements.txt .
 RUN python -m pip install -r requirements.txt
@@ -14,10 +15,19 @@ RUN python -m pip install -r requirements.txt
 WORKDIR /app
 COPY . /app
 
+##### Add environment variables for input and output folders of subtitles and text files
+ENV INPUT_FOLDER examples
+ENV OUTPUT_FOLDER ss
+
+# mount folders to the container
+RUN --mount=type=bind,target=$INPUT_FOLDER 
+RUN --mount=type=bind,target=$OUTPUT_FOLDER,rw=true
+
 # Creates a non-root user with an explicit UID and adds permission to access the /app folder
 # For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
-RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
+RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app 
 USER appuser
 
-# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD ["python", "srt2text.py"]
+##### Run the file with with the folders as arguments and copy output 
+CMD ["sh", "-c","python srt2text.py -f $INPUT_FOLDER -d $OUTPUT_FOLDER "]
+COPY /$OUTPUT_FOLDER /"$OUTPUT_FOLDER"
--- srt2text.py
+++ srt2text.py
@@ -1,10 +1,15 @@
 import re
+import os
 import argparse
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument("subtitle_file", help="name of the subtitle to extract text from")
-parser.add_argument("-o","--output-file", help="the name of the resulting text file")
+input_arguments = parser.add_mutually_exclusive_group(required=True)
+input_arguments.add_argument("-s","--subtitle_file", help="name of the subtitles file to extract text from")
+input_arguments.add_argument("-f","--subtitle_folder", help="name of the folder containing subtitles files to extract text from")
+output_arguments = parser.add_mutually_exclusive_group()
+output_arguments.add_argument("-o","--output-file", help="the name of the resulting text file")
+output_arguments.add_argument("-d","--output-folder", help="the name of the folder for the resulting text files")
 parser.add_argument("-t", "--timing-save",
                     help="save subtitle timing info into the following file path: name of input_file + \"_srt_info.txt\"", action="store_true")
 print_options = parser.add_mutually_exclusive_group()
@@ -19,43 +24,57 @@ srt_info_file = None
 srt_info = None
 output_line = ""
 
-assert(Path(arguments_parsed.subtitle_file).suffix == ".srt"),"Input file provided is not a .srt file"
-subtitle_file_name = arguments_parsed.subtitle_file 
-if arguments_parsed.timing_save:
-    srt_info_file = subtitle_file_name.replace('.srt', "_srt_info.txt", 1)
-text_file_name = arguments_parsed.output_file if arguments_parsed.output_file else arguments_parsed.subtitle_file.replace(".srt", ".txt", 1)
+if arguments_parsed.subtitle_folder:
+    folder = arguments_parsed.subtitle_folder
+    subtitle_files = os.listdir(arguments_parsed.subtitle_folder)
+elif arguments_parsed.subtitle_file:
+    folder = "."
+    subtitle_files = [arguments_parsed.subtitle_file]
 
-# open srt file and create text file and (optional) srt timing file
-with open(subtitle_file_name, 'r') as srt_file,  open(text_file_name, 'w') as text_file:
-    if srt_info_file:
-        srt_info = open(srt_info_file, "w")
-    # process subtitle file line by line
-    for line in srt_file:    
-         # if line consists of single integer ==> sequence number ==> write inside timing file
-        if re.match(r'^\d+\n$', line):
-            just_read_num = True # mark down that integer was just read
-            just_read_interval = False
-            output_line = line
-            if srt_info:
-                srt_info.write(output_line)
-         # if line consists of time interval ==> subtitle timing ==> write inside timing file
-        elif just_read_num and re.match(r'^\d{2,}:[0-5][0-9]:[0-5][0-9],\d{3} --> \d{2,}:[0-5][0-9]:[0-5][0-9],\d{3}\n$', line):
-            just_read_num = False 
-            just_read_interval = True # mark down that time interval was just read
-            output_line = line
-            if srt_info: 
-                srt_info.write(output_line + '\n')
-        # if interval was just read (meaning number was read two lines ago), current line is subtile text
-        # or space between subtitle timing block  ==> write line to text file
-        elif just_read_interval:
-            # if special options chose, current line will not start new line at end
-            if arguments_parsed.all_in_one_line or arguments_parsed.interval_in_one_line and not line in '\n': 
-                output_line = line.replace('\n', '', 1)
-                # if one line needed, current text is positioned a space away from previous text 
-                if arguments_parsed.all_in_one_line:
-                     output_line = (" %s" % output_line)
-            else:
+if arguments_parsed.output_folder:
+    output_folder = arguments_parsed.output_folder
+elif arguments_parsed.subtitle_file:
+    output_folder = "."
+
+assert(not (arguments_parsed.subtitle_file and arguments_parsed.output_folder) and not (arguments_parsed.subtitle_folder and arguments_parsed.output_file)), "Input and output types are incompatible"
+for file in subtitle_files:
+    assert(Path(file).suffix == ".srt"),"Input file provided is not a .srt file or may be a directory"
+    subtitle_file_name = file 
+    if arguments_parsed.timing_save:
+        srt_info_file = subtitle_file_name.replace('.srt', "_srt_info.txt", 1)
+    text_file_name = arguments_parsed.output_file if arguments_parsed.output_file else file.replace(".srt", ".txt", 1)
+
+    # open srt file and create text file and (optional) srt timing file
+    with open(folder + "/" + subtitle_file_name, 'r') as srt_file,  open(output_folder + "/" + text_file_name, 'w') as text_file:
+        if srt_info_file:
+            srt_info = open(output_folder + "/" + srt_info_file, "w")
+        # process subtitle file line by line
+        for line in srt_file:    
+            # if line consists of single integer ==> sequence number ==> write inside timing file
+            if re.match(r'^\d+\n$', line):
+                just_read_num = True # mark down that integer was just read
+                just_read_interval = False
                 output_line = line
-            text_file.write(output_line)
-    if srt_info_file:
-        srt_info.close()
+                if srt_info:
+                    srt_info.write(output_line)
+            # if line consists of time interval ==> subtitle timing ==> write inside timing file
+            elif just_read_num and re.match(r'^\d{2,}:[0-5][0-9]:[0-5][0-9],\d{3} --> \d{2,}:[0-5][0-9]:[0-5][0-9],\d{3}\n$', line):
+                just_read_num = False 
+                just_read_interval = True # mark down that time interval was just read
+                output_line = line
+                if srt_info: 
+                    srt_info.write(output_line + '\n')
+            # if interval was just read (meaning number was read two lines ago), current line is subtile text
+            # or space between subtitle timing block  ==> write line to text file
+            elif just_read_interval:
+                # if special options chose, current line will not start new line at end
+                if arguments_parsed.all_in_one_line or arguments_parsed.interval_in_one_line and not line in '\n': 
+                    output_line = line.replace('\n', '', 1)
+                    # if one line needed, current text is positioned a space away from previous text 
+                    if arguments_parsed.all_in_one_line:
+                        output_line = (" %s" % output_line)
+                else:
+                    output_line = line
+                text_file.write(output_line)
+        if srt_info_file:
+            srt_info.close()
